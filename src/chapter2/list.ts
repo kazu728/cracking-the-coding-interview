819cc497efe59ddd33e7fee3375959d20f218d66
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2022-03-19 17:28:41 -0700

    feat: linked list

---
+++ src/chapter2/list.ts
@@ -0,0 +1,23 @@
+import { assertEquals } from "../deps.ts";
+
+export class Linkedlist<T> {
+  constructor(public readonly value: T, public next?: Linkedlist<T>) {}
+}
+
+export const genLinkedlist = <T>(l: T[]): Linkedlist<T> => {
+  const go = (linkedList: Linkedlist<T>, l: T[], i: number): Linkedlist<T> => {
+    const currentValue = l[i];
+
+    if (currentValue === undefined) return linkedList;
+    linkedList.next = go(new Linkedlist<T>(currentValue), l, i + 1);
+
+    return linkedList;
+  };
+
+  return go(new Linkedlist<T>(l[0]), l, 1);
+};
+
+assertEquals(
+  genLinkedlist([1, 2, 3]),
+  new Linkedlist(1, new Linkedlist(2, new Linkedlist(3)))
+);
