e1309b11805721a4e312b61497daee5820d02430
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2023-01-13 00:05:10 +0900

    refactor: reformat

--- tests/Chapter1/Q1Test.elm
+++ tests/Chapter1/Q1Test.elm
@@ -8,6 +8,12 @@ import Test exposing (..)
 suite : Test
 suite =
     describe "Chapter1.Q1"
-        [ test "is unique" <| \_ -> Chapter1.Q1.isUniqueChar "abcdefghijklmnopqrstuvwxyz" |> Expect.equal True
-        , test "is not unique" <| \_ -> Chapter1.Q1.isUniqueChar "abcnvakehrgoijerwapgfojrmegmreognmi" |> Expect.equal False
+        [ test "is unique" <|
+            \_ ->
+                Chapter1.Q1.isUniqueChar "abcdefghijklmnopqrstuvwxyz"
+                    |> Expect.equal True
+        , test "is not unique" <|
+            \_ ->
+                Chapter1.Q1.isUniqueChar "abcnvakehrgoijerwapgfojrmegmreognmi"
+                    |> Expect.equal False
         ]
